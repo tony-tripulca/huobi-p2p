0d14b1c7962313f0d5303cccbd8fb5894e92152e
Fernand Anthony Tripulca <tony.tripulca@gmail.com>
2022-09-09 16:44:05 +0800

    Fetch p2p public ads

--- server/app.js
+++ server/app.js
@@ -136,3 +136,51 @@ app.get("/p2p-orders", async function (req, res) {
     result: result,
   });
 });
+
+app.get("/p2p-ads", async function (req, res) {
+  let protocol = "https://";
+  let host = "otc-api.trygofast.com";
+  let endpoint = `/api/public/ad-get`;
+
+  let timestamp = new Date();
+
+  let payload = {
+    accessKey: ACCESS_KEY,
+    signatureMethod: "HmacSHA256",
+    signatureVersion: 2.1,
+    timestamp: timestamp.toISOString().substring(0, 19),
+  };
+
+  let query = [];
+  for (let data in payload) {
+    query.push(data + "=" + encodeURIComponent(payload[data]));
+  }
+
+  let p = query.sort().join("&");
+
+  let meta = ["GET", host, endpoint, p].join("\n");
+  let hash = HmacSHA256(meta, SECRET_KEY);
+  let signature = encodeURIComponent(CryptoJS.enc.Base64.stringify(hash));
+
+  p += `&signature=${signature}`;
+
+  let result = await axios
+    .get(`${protocol}${host}${endpoint}?${p}`)
+    .then(function (response) {
+      let res = {
+        data: response.data,
+        config: response.config,
+        headers: response.headers,
+      };
+
+      return res;
+    })
+    .catch(function (error) {
+      return error;
+    });
+
+  res.json({
+    signed: p,
+    result: result,
+  });
+});
